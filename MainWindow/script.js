9edc64a0919a179e8f7c8c8a3ce541479848402b
JakubJKrawczyk <zrespawnieni@gmail.com>
2022-09-06 19:59:50 +0200

    Modules Added

--- MainWindow/script.js
+++ MainWindow/script.js
@@ -1,3 +1,12 @@
-$(document).ready(() => {
-    
-})
+
+var calendar = [31,28,30,31,30,31,31,31,30,31,30,31]
+
+function IncludeGroups(){
+window.alert("i huj");
+
+}
+
+function IncludeEvents(){
+    $("#content-container").load("./modules/Events.html");
+}
+
