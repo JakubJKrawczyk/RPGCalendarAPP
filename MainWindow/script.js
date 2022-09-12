e6004d0bfcc9e3c1f8962c39b7cdda29087f6832
JakubJKrawczyk <zrespawnieni@gmail.com>
2022-09-12 22:38:47 +0200

    W tym commit'cie wykonałem: - wystylizowałem przyciski menu - zaprojektowałem i wdrożyłem animacje - wdrożyłem funkcjonalność przycisków

--- MainWindow/script.js
+++ MainWindow/script.js
@@ -1,12 +1,71 @@
 
-var calendar = [31,28,30,31,30,31,31,31,30,31,30,31]
+let isMouseOver = false;
 
-function IncludeGroups(){
-window.alert("i huj");
+
+
+
+
+// include functions
+function IncludeAllGroups(){
+    $("#content-container").load("./modules/Groups/AllGroups/AllGroups.html");
 
 }
+function IncludeMyGroups(){
+    $("#content-container").load("./modules/Groups/MyGroups/MyGroups.html");
 
-function IncludeEvents(){
-    $("#content-container").load("./modules/Events.html");
 }
+function IncludeMyAccount(){
+    $("#content-container").load("./modules/MyAccount/MyAccount.html");
+
+}
+function IncludeGeneralCalendar(){
+    $("#content-container").load("./modules/Calendars/GeneralCalendar/GeneralCalendar.html");
+
+}
+function IncludeMyCalendar(){
+    $("#content-container").load("./modules/Calendars/MyCalendar/MyCalendar.html");
+
+}
+function IncludeAllEvents(){
+    $("#content-container").load("./modules/Events/AllEvents/AllEvents.html");
+}
+function IncludeMyEvents(){
+    $("#content-container").load("./modules/Events/MyEvents/MyEvents.html");
+}
+
+
+// Menu Events section
+
+function ShowGroupsMenu(){
+    document.getElementById("groupsHiddenMenuDiv").style.height = "150px";
+}
+
+function ShowEventsMenu(){
+    document.getElementById("eventsHiddenMenuDiv").style.height = "150px"; 
+}
+
+function ShowCalendarsMenu(){
+    document.getElementById("calendarHiddenMenuDiv").style.height = "150px";  
+}
+
+function OnMouseClick(){
+    if(!isMouseOver){
+        document.getElementById("groupsHiddenMenuDiv").style.height = "48px";
+        document.getElementById("eventsHiddenMenuDiv").style.height = "48px"; 
+        document.getElementById("calendarHiddenMenuDiv").style.height = "48px";  
+
+        
+
+        
+
+    }
+    
+}
+
+function ChangeMouseOver(){
+    isMouseOver = isMouseOver == true ? false : true;
+}
+
+
+
 
